9389ad5db95e56a7cc395a5683f22afbdc15280b
Lakshay-Goyal-19 <gyllakshay@gmail.com>
2025-05-27 22:02:04 +0530

    first commit

---
+++ src/main/java/com/abes/fdms/Main.java
@@ -0,0 +1,7 @@
+package com.abes.fdms;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/com/abes/fdms/MainTest.java
@@ -0,0 +1,7 @@
+package com.abes.fdms;
+
+public class MainTest {
+		public static void main(String[] args) {
+		System.out.println("Testing for Main class");
+	}
+}
