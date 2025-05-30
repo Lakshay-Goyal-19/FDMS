b5c7ef6e015f84d48e5c7ad517a47b7f20d6cc4f
manuvarshneyy <manuvarshney633@gmail.com>
2025-05-30 12:24:39 +0530

    dto 3 files

---
+++ src/main/java/com/abes/fdms/dto/ManagerDto.java
@@ -0,0 +1,21 @@
+package com.abes.fdms.dto;
+import com.abes.fdms.annotation.RoleCheck;
+
+@RoleCheck(role = "Manager")
+public class ManagerDto extends UserDto {
+    private String password;
+
+    public ManagerDto(String id, String name, String email, String phoneNumber, String password) {
+        super(id, name, email, phoneNumber);
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public void showProfile() {
+        System.out.println("Manager ID: " + id + ", Name: " + name + ", Email: " + email + ", Phone: " + phoneNumber);
+    }
+}
